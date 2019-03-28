5e0d93f0bd658a47e5bc4dfb7f2db31f811ee2b7
katsuki hanawa <katsuki8hanawa6@gmail.com>
2019-03-28 11:48:10 +0900

    read files

---
+++ app.py
@@ -0,0 +1,8 @@
+import readFiles
+
+def main():
+    rf = readFiles.ReadFiles()
+    rf.loadFiles()
+
+if __name__ == "__main__":
+    main()
---
+++ readFiles.py
@@ -0,0 +1,28 @@
+import glob
+import numpy as np
+import sys
+import os
+
+class ReadFiles:
+    path_to_files = ""
+    users_data = None
+    def __init__(self):
+        self.path_to_files = os.getcwd()
+        self.users_data = []
+
+    def loadFiles(self):
+        level1 = glob.glob(os.path.join(self.path_to_files, 'txtdata', '*'))
+        for f in level1:
+            level2 = glob.glob(os.path.join(f, "*"))
+            for f2 in level2:
+                level3 = glob.glob(os.path.join(f2, '*.txt'))
+                print(os.path.basename(f2))
+                username = os.path.basename(f2)
+                self.users_data.append({"username": username, "contents": []})
+                for f3 in level3:
+                    with open(f3) as contens:
+                        self.users_data[-1]["contents"].append(contens.read())
+        print(self.users_data)
+
+    def outputFiles(self):
+        pass
